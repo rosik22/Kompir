ed9546f75b8576d2abe4dd02d3531384face28d2
Rositsa Karapetrova <rosik22@abv.bg>
2019-07-15 04:58:45 +0300

    promeni

--- Exercise/skr.cpp
+++ Exercise/skr.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    for(char c = '0'; c <= '9'; c++){
-        cout<<c<<'\t'<<(int)c<<'\n';
+int gcd(unsigned a, unsigned b){
+    if (a==0) return b;
+    else gcd(b%a, a);
+}
+
+int fgcd(int a[], int b){
+    int result = a[0];
+    for(int i=1; i<b; i++){
+        result = gcd(a[i], result);
     }
+}
+
+int main(){
+    int a[] = {9,15,27};
+
+    cout<< fgcd(a,3)<<endl;
 }
