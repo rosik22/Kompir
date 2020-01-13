44886c91a2848f25c6ec53100adcf05ebd3b39c8
rosi k <rosik22@abv.bg>
2020-01-13 22:16:18 +0200

    changed scroll & added text

--- data_structures_3rd_sem/hw/vik.cpp
+++ data_structures_3rd_sem/hw/vik.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 using namespace std;
 
-typedef struct node * po;
-struct node{
+typedef struct node *po;
+struct node
+{
     int data;
     po left;
     po right;
 };
 
-class Heap{
+class Heap
+{
 public:
     po build(int num);
     void infix(po help);
-   /* left(p);
+    /* left(p);
     right(p);
     parent(p);
     root();
@@ -28,25 +30,31 @@ protected:
     po right;
 };
 
-po Heap::build(int num){
-    po temp; int c;
-    if(num>0){
-        int nl = num/2;
-        int nd = num-nl-1;
-        cin>>c;
+po Heap::build(int num)
+{
+    po temp;
+    int c;
+    if (num > 0)
+    {
+        int nl = num / 2;
+        int nd = num - nl - 1;
+        cin >> c;
         temp = new node;
-        temp -> data = c;
-        temp -> left = build(nl);
-        temp -> right = build(nd);
+        temp->data = c;
+        temp->left = build(nl);
+        temp->right = build(nd);
         return temp;
     }
-    else return NULL;
+    else
+        return NULL;
 }
 
-void Heap::infix(po help){
-    if(help){
+void Heap::infix(po help)
+{
+    if (help)
+    {
         infix(help->left);
-        cout<<help->data<<" ";
+        cout << help->data << " ";
         infix(help->right);
     }
 }
@@ -67,13 +75,15 @@ void print(po root, int space)
 	print(root->left, space);
 }*/
 
-int main(){
+int main()
+{
     int num;
-    cin>>num;
+    cin >> num;
     Heap *root = new Heap();
     root->build(num);
-    
-    cout<<endl<<"infix: ";
+
+    cout << endl
+         << "infix: ";
     infix(root);
     return 0;
 }
