80153b12535fe64d1a071774ab689968e29c17d1
rosi k <rosik22@abv.bg>
2020-02-25 12:44:07 +0200

    .

---
+++ 4th_PYTHON/first.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a > 0:
+    d = b**2-4*a*c
+    if d > 0:
+        print("x1 and x2 exist")
+    else:
+        print("x1 and x2 don't exist")
+else:
+    if b > 0:
+        x = -c/b
+        print(x)
+    else:
+        print("Weird")
